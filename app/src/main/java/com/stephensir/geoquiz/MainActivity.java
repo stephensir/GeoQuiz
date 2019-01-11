53192bc532bc3b25e77899a726f76ce297477880
Stephen <StephenSir@gmail.com>
2019-01-11 19:49:42 +0800

    lifecycle

--- app/src/main/java/com/stephensir/geoquiz/MainActivity.java
+++ app/src/main/java/com/stephensir/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.stephensir.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,11 +11,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mYesButton;
     private Button mNoButton;
+    private String TAG = "GeoQuiz===>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"onCreate");
 
         mYesButton = findViewById(R.id.yes_button);
         mYesButton.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +34,35 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
             }
         });
-    }
+    } //onCreate()
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    } //onStart()
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    } //onResume()
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    } //onPause()
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    } //onStop()
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestory");
+    } //onDestory()
 }
