5ad905cf1c3ce554dd7b1f8d2378a8deb237f2dd
Stephen <StephenSir@gmail.com>
2019-01-12 20:37:43 +0800

    New branch

--- app/src/main/java/com/stephensir/geoquiz/MainActivity.java
+++ app/src/main/java/com/stephensir/geoquiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     private Button mYesButton;
     private Button mNoButton;
     private String TAG = "GeoQuiz===>";
+
+    private Question[] mQuestionBank = new Question[] {
+            // Format
+            new Question(getResources().getString(R.string.question_text1), true),
+            new Question(getResources().getString(R.string.question_text2), true),
+            new Question(getResources().getString(R.string.question_text3), false),
+    };
+    private TextView txtOutput;
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +31,44 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"onCreate");
         Log.d(TAG,"Updated");
 
+        txtOutput = findViewById(R.id.txtOutput);
+
         mYesButton = findViewById(R.id.yes_button);
         mYesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(mQuestionBank[mCurrentIndex].getAnswer(),true);
             }
         });
         mNoButton = findViewById(R.id.no_button);
         mNoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(mQuestionBank[mCurrentIndex].getAnswer(),false);
             }
         });
     } //onCreate()
+    private void checkAnswer(boolean _ans, boolean _user_ans){
+        if (_ans == _user_ans){
+            Toast.makeText(MainActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void btn_previous_click(View v){
+        // index reach 0
+        if (mCurrentIndex==0){
+            return;
+        }
+        mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+        txtOutput = findViewById(R.id.txtOutput);
+        txtOutput.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    }
+    public void btn_next_click(View v){
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        txtOutput = findViewById(R.id.txtOutput);
+        txtOutput.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    }
 
     @Override
     protected void onStart() {
---
+++ app/src/main/java/com/stephensir/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.stephensir.geoquiz;
+
+public class Question {
+    // Members
+    private String question;
+    private boolean answer;
+
+    // Constructor
+    public Question(String _q, boolean _a) {
+        question = _q;
+        answer = _a;
+    }
+
+    // Setter and Getter
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String _q) {
+        question = _q;
+    }
+
+    public boolean getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean _a) {
+        answer = _a;
+    }
+
+}
